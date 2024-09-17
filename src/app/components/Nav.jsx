abc1645f1c6c1140fdbe26a25d2da928fa42df8c
ktunchar <maikittitee1615@gmail.com>
2024-09-17 22:08:37 +0700

    feat: nav bar

---
+++ src/app/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './Nav.module.css'; // Assuming you will create a CSS module
+
+function Nav() {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navItems}>
+        <li className={styles.logo}>
+          <Link href="/">
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.jpg" alt="Logo" className={styles.logoImage} />
+          </Link>
+        </li>
+        <div className={styles.authLinks}>
+          <li><Link href="sign-in">Sign In</Link></li>
+          <li><Link href="sign-up">Sign Up</Link></li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
